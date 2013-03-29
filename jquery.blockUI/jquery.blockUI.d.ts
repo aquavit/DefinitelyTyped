63dd259395d5fe0d136dccab68644fbffd8d33d4
unknown <tabuchi_naoshi@PC201204-001.trek.local>
2013-03-29 17:29:16 +0900

    Added: base64/base64.d.ts

--- jquery.blockUI/jquery.blockUI.d.ts
+++ jquery.blockUI/jquery.blockUI.d.ts
@@ -1,4 +1,4 @@
-﻿// Typescript type definitions for jQuery.i18n.properties 2.55.0 byv
+﻿// Typescript type definitions for jQuery.i18n.properties 2.55.0 by M. Alsup
 // Project: http://malsup.com/jquery/block/
 // Definitions by: Naoshi Tabuchi
 // Definitions: https://bitbucket.org/ntabee/definitelytyped-clone.git
