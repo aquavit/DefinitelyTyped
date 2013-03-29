838e84e05f742a0482d3b69c69c7275fb75f95b0
unknown <tabuchi_naoshi@PC201204-001.trek.local>
2013-03-29 09:59:06 +0900

    jquery.blockUI, jquery.i18n

---
+++ jquery.blockUI/jquery.blockUI.d.ts
@@ -0,0 +1,26 @@
+﻿// Typescript type definitions for jQuery.i18n.properties 2.55.0 byv
+// Project: http://malsup.com/jquery/block/
+// Definitions by: Naoshi Tabuchi
+// Definitions: https://bitbucket.org/ntabee/definitelytyped-clone.git
+
+
+/// <reference path='../jquery/jquery.d.ts'/>
+
+interface BlockUI {
+    (opts?: any): void;
+    defaults: any;
+}
+interface UnblockUI {
+    (opts?: any): void;
+    defaults: any;
+}
+interface JQuery {
+    blockUI: BlockUI;
+    unblockUI : UnblockUI;
+}
+
+interface JQueryStatic {
+    blockUI: BlockUI;
+    unblockUI: UnblockUI;
+}
+
---
+++ jquery.i18n/jquery.i18n.properties.d.ts
@@ -0,0 +1,20 @@
+﻿// Typescript type definitions for jQuery.i18n.properties 1.0.9 by Nuno Fernandes
+// Project: http://http://code.google.com/p/jquery-i18n-properties/
+// Definitions by: Naoshi Tabuchi
+// Definitions: https://bitbucket.org/ntabee/definitelytyped-clone.git
+
+
+/// <reference path='../jquery/jquery.d.ts'/>
+
+interface I18NProperties {
+    properties: (settings) => void;
+    prop: (key: string) => string;
+}
+
+
+interface JQuery {
+    i18n: I18NProperties;
+}
+interface JQueryStatic {
+    i18n: I18NProperties;
+}
