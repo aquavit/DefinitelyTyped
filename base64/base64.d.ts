1eb24daf3da36ee1257a8b59eaf93d57a4454e77
unknown <tabuchi_naoshi@PC201204-001.trek.local>
2013-03-29 17:32:58 +0900

    Added: base64/base64.t.ds

---
+++ base64/base64.d.ts
@@ -0,0 +1,10 @@
+﻿// Typescript type definitions for base64 2.6 by dankogai
+// Project: https://github.com/dankogai/js-base64
+// Definitions by: Naoshi Tabuchi
+// Definitions: https://bitbucket.org/ntabee/definitelytyped-clone.git
+
+interface IBase64 {
+    encode: (s: string) => string;
+    decode: (s: string) => string;
+}
+declare var Base64: IBase64;
